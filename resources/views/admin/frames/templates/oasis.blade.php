c01bff145182072f684bbbaa976125c25cc059cf
Fikoabdgst <fikoabdigusti15@gmail.com>
2025-07-23 12:03:14 +0700

    add frame

--- resources/views/admin/frames/templates/oasis.blade.php
+++ resources/views/admin/frames/templates/oasis.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full h-full relative bg-blue-0 overflow-hidden shadow-[0_0_20px_rgba(0,0,0,0.3)]">
-    <img src="{{ asset('stitch/bg.jpeg') }}" class="w-[190px] h-[500px] opacity-80 z-10" />
+    <img src="{{ asset('oasis/oasis.jpeg') }}" class="w-[190px] h-[500px] opacity-80 z-10" />
 
     <!-- First Photo Slot -->
     <div class="absolute top-[20px] left-[10px] w-[calc(100%-20px)] h-[120px]" data-photo-index="0">
@@ -49,42 +49,48 @@
         </div>
     </div>
 
-    <div class="absolute z-[30] bottom-[385px] left-2 w-full text-center pointer-events-none">
-        <img src="{{ asset('stitch/tag.png') }}" alt="Nj1" style="transform: translateX(-10px);"
-            class="h-100px] w-auto">
+    <div class="absolute z-[30] bottom-[430px] left-2 w-full text-center pointer-events-none">
+        <img src="{{ asset('oasis/tag.png') }}" alt="Nj1" style="transform: translateX(35px);"
+            class="h-[100px] w-auto">
     </div>
 
-    <div class="absolute z-[30] bottom-[355px] left-2 w-full text-center pointer-events-none">
-        <img src="{{ asset('stitch/stch1.png') }}" alt="Nj2" style="transform: translateX(-57px) rotate(50deg);"
-            class="h-[105px] w-auto">
-    </div>
-
-    <div class="absolute z-[30] bottom-[300px] left-2 w-full text-center pointer-events-none">
-        <img src="{{ asset('stitch/stch7.png') }}" alt="Nj2" style="transform: translateX(125px);"
+    <div class="absolute z-[30] bottom-[405px] left-2 w-full text-center pointer-events-none">
+        <img src="{{ asset('oasis/oasis4.png') }}" alt="Nj2" style="transform: translateX(-30px) rotate(-20deg);"
             class="h-[140px] w-auto">
     </div>
 
-
-    <div class="absolute z-[30] bottom-[175px] left-2 w-full text-center pointer-events-none">
-        <img src="{{ asset('stitch/stch4.png') }}" alt="Nj1" style="transform: translateX(-20px);"
-            class="h-[130px] w-auto">
-    </div>
-    <div class="absolute z-[30] bottom-[190px] left-2 w-full text-center pointer-events-none">
-        <img src="{{ asset('stitch/stch5.png') }}" alt="Nj2" style="transform: translateX(125px);"
-            class="h-[65px] w-auto">
+    <div class="absolute z-[30] bottom-[310px] left-2 w-full text-center pointer-events-none">
+        <img src="{{ asset('oasis/oasis7.png') }}" alt="Nj2" style="transform: translateX(120px) rotate(-20deg);"
+            class="h-[90px] w-auto">
     </div>
 
-    <div class="absolute z-[30] bottom-[60px] left-2 w-full text-center pointer-events-none">
-        <img src="{{ asset('stitch/stch6.png') }}" alt="Nj3" style="transform: translateX(-10px);"
-            class="h-[100px] w-auto">
+
+    <div class="absolute z-[40] bottom-[200px] left-2 w-full text-center pointer-events-none">
+        <img src="{{ asset('oasis/oasis5.png') }}" alt="Nj1" style="transform: translateX(-20px) rotate(0deg);"
+            class="h-[30px] w-auto">
     </div>
-    <div class="absolute z-[30] bottom-[-80px] left-2 w-full text-center pointer-events-none">
-        <img src="{{ asset('stitch/stch2.png') }}" alt="Nj7" class="w-[200px]"
-            style="transform: translateX(60px) rotate(-10deg);">
+    <div class="absolute z-[30] bottom-[200px] left-2 w-full text-center pointer-events-none">
+        <img src="{{ asset('oasis/oasis6.png') }}" alt="Nj1" style="transform: translateX(-30px) rotate(30deg);"
+            class="h-[90px] w-auto">
+    </div>
+
+
+    <div class="absolute z-[30] bottom-[-20px] left-2 w-full text-center pointer-events-none">
+        <img src="{{ asset('oasis/oasis1.png') }}" alt="Nj3" style="transform: translateX(-50px);"
+            class="h-[180px]">
+    </div>
+    <div class="absolute z-[30] bottom-[82px] left-2 w-full text-center pointer-events-none">
+        <img src="{{ asset('oasis/oasis3.png') }}" alt="Nj7" class="w-[120px]"
+            style="transform: translateX(110px) rotate(-20deg);">
 
     </div>
-    <div class="absolute z-[50] bottom-[-10px] left-[60px] pointer-events-none">
-        <img src="{{ asset('panoricam.png') }}" alt="Logo" class="h-[60px] w-auto">
+    <div class="absolute z-[20] bottom-[-25px] left-2 w-full text-center pointer-events-none">
+        <img src="{{ asset('oasis/oasis2.jpeg') }}" alt="Nj7" class="w-[100px]"
+            style="transform: translateX(90px) rotate(-10deg);">
+
+    </div>
+    <div class="absolute z-[50] bottom-[35px] left-[65px] pointer-events-none">
+        <img src="{{ asset('panoricam2.png') }}" alt="Logo" class="h-[10px] w-auto">
     </div>
 
 
